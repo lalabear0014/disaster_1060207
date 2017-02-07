ae0f15ad1c4a553fffed05fd6c26c406ea9b1971
Air <air@Airs-MacBook.local>
2017-02-07 22:58:26 +0800

    Generate model category & db:migrate

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,6 @@ class Event < ApplicationRecord
 	validates_presence_of :title
 
 	has_many :messages
+	belongs_to :category
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207132015) do
+ActiveRecord::Schema.define(version: 20170207145327) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
   create_table "messages", force: :cascade do |t|
