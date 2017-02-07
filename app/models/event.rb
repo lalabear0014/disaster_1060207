f29f4eaa717c87c73c808c348a8aac1f495a8525
Air <air@Airs-MacBook.local>
2017-02-07 21:31:18 +0800

    one-to-many

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+
 	validates_presence_of :title
+
+	has_many :messages
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- 	resources :events
+ 	resources :events do
+  		resources :messages, :controller => "event_messages"
+  	end
 
  	get "events" => "events#index"
     root 'events#index'
