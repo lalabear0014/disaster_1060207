b4025227a8fc42b259ed3507d8ef3743d605db74
Air <air@Airs-MacBook.local>
2017-02-07 20:17:57 +0800

    CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,55 @@
 class EventsController < ApplicationController
+
+	# GET /events/index
+	# GET /events
+	def index
+		@events = Event.all
+	end
+
+	# GET /events/show/:id
+	def show
+		@event = Event.find( params[:id] )
+	end
+
+	# GET /events/new
+	def new
+		@event = Event.new
+	end
+
+	# POST /events/create
+ 	def create
+		@event = Event.new( event_params )
+		@event.save
+
+		# 告訴瀏覽器 HTTP code: 303
+		redirect_to :action => :index
+	end
+
+	# GET /events/edit/:id
+	def edit
+		@event = Event.find( params[:id] )
+	end
+
+	# POST /events/update/:id
+	def update
+		@event = Event.find( params[:id] )
+		@event.update( event_params )
+
+		redirect_to :action => :show, :id => @event
+	end
+
+	# GET /events/destroy/:id
+	def destroy
+		@event = Event.find( params[:id] )
+		@event.destroy
+
+		redirect_to :action => :index
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:title, :description)
+ 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ 	get "events" => "events#index"
+    root 'events#index'
+
+    match ':controller(/:action(/:id(.:format)))', :via => :all
+
 end
